18d0faf7c8b7ed71b582d5e3d6dfb31d857d9806
Mina Lee <mal0070@naver.com>
2025-07-22 20:24:38 +0900

    chore: add GA

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import ReactGA from "react-ga4";
+import { useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import HeroSection from "./components/home/HeroSection";
 import FeaturesSection from "./components/home/FeaturesSection";
@@ -24,6 +26,14 @@ const MainPage = () => (
   </>
 );
 
+// 페이지 이동 시마다 pageview 전송
+function usePageView() {
+  const location = useLocation();
+  React.useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+}
+
 const App = () => {
   const [isSignUpOpen, setIsSignUpOpen] = useState(false);
   const [isContactOpen, setIsContactOpen] = useState(false);
@@ -31,6 +41,8 @@ const App = () => {
   useEffect(() => {
     // 기존 애니메이션 및 모달 핸들링 생략 가능
   }, []);
+
+  usePageView();
 
   return (
     <Router>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-YNBQXF9B0P"); // 측정 ID로 교체
+
+// 페이지 이동 추적 예시
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
