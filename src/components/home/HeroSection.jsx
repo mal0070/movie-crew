221b345233287f8c1d5cdb77dba8d73cc34e02b0
Mina Lee <mal0070@naver.com>
2025-08-26 15:26:13 +0900

    design: change button locate on HeroSection

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -23,18 +23,18 @@ const HeroSection = () => {
         경험이 없어도, 인맥이 없어도, 당신의 꿈을 함께 만들어가요.
         </p>
         <div className="flex flex-wrap gap-4 justify-center">
-          <button
-            onClick={() => setIsPostModalOpen(true)}
-            className="px-6 py-3 text-white rounded-full bg-gradient-to-r from-orange-500 to-yellow-400 hover:shadow-xl text-lg"
-          >
-            크루 찾기 시작하기
-          </button>
           <Link
             to="/opening"
             className="px-6 py-3 bg-white border-2 border-orange-500 text-orange-500 rounded-full hover:bg-orange-500 hover:text-white text-lg"
           >
             구인정보 둘러보기
           </Link>
+          <button
+            onClick={() => setIsPostModalOpen(true)}
+            className="px-6 py-3 text-white rounded-full bg-gradient-to-r from-orange-500 to-yellow-400 hover:shadow-xl text-lg"
+          >
+            크루 찾기 시작하기
+          </button>
         </div>
         {/* 공고 작성 모달 (PostModal) - 추후 구현 */}
         <WriteModal open={isPostModalOpen} onClose={() => setIsPostModalOpen(false)} />
